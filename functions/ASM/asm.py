ee62bfde6cc44c2769c61623e44f7fd1e2ce03a8
Steve Brooks <stephen@Stephen-MacBook-Air.local>
2017-04-03 16:27:35 -0700

    ASM Cleanup

--- functions/ASM/asm.py
+++ functions/ASM/asm.py
@@ -1,7 +1,15 @@
+'''
+MAKES ASM FROM GDB
+'''
+import csv
 import fiona
 
+POLENAME = lambda x: ('P_' + str(x['id']) if not x['properties']['FacilityID']
+                      else 'P_' + x['properties']['FacilityID'])
+POLEASS = lambda x: str(x['properties']['Height']) + '-' + str(x['properties']['Class'])
 
 def make_structure(support):
+    'From GDB Suport entry, make an ASM line'
     properties = support['properties']
     out = {}
     out['AssemblyName'] = str(properties['Height']) + '-' + str(properties['Class'])
@@ -9,8 +17,10 @@ def make_structure(support):
     out['SRIPOLEID'] = str(properties['GlobalID'])
     return out
 
-STRUCTURE_LAYER = 'SupportStructure'
-GDB = 'GDB/Export_Feb01.gdb'
-
-STRUCTURES = (make_structure(structure)
-              for structure in fiona.open(GDB, layer=STRUCTURE_LAYER))
+def make_asm(gdb='GDB/Export_Feb01.gdb', export='ASM'):
+    "From the GDB, read the layer, make the ASM FILE"
+    asm = [[POLENAME(element), POLEASS(element), 1, None, None, None, None, None]
+           for element in fiona.open(gdb, layer='SupportStructure')]
+    with open(export + '.asm', 'w') as asm_file:
+        writer = csv.writer(asm_file)
+        writer.writerows(asm)
